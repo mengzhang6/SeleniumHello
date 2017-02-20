a45e12b4b0937e21b6a8cb56fe7c1dd3d5f1d017
TonyMengTT <1213252320@qq.com>
2017-02-20 21:53:57 +0800

    截图功能
    添加需要导入的包

--- src/meng/selenium/test3/APITest.java
+++ src/meng/selenium/test3/APITest.java
@@ -1,11 +1,16 @@
 package meng.selenium.test3;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
